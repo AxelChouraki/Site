e378050116e11e87c4a9580033063c280ca9c757
Axel Chouraki <axel.chouraki@centrale-marseille.fr>
2020-12-14 13:53:55 +0100

    Inital Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+    <!DOCTYPE html>
+    <html>
+        <head>
+        	<meta charset="UTF-8">
+        	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+            <title>Accueil stylé</title>
+            <!-- <link href="style.css rel="stylesheet" /> -->
+            <style>
+                body{background-color:silver;
+                        padding-left: 5%;}             
+                .grand_centre {
+                   color: blue;
+                   font-family: verdana;
+                   font-size: 200%;
+                   text-align: center;
+                   }
+                .image {text-align:center;}
+            </style>
+        </head>
+     
+
+        <body>
+
+            <div class="grand_centre">Bienvenue sur LE site</div
+            <br />
+            <a href="Test.html">Go Multi</a>
+            <br />
+            <a href="bin/chat-server.php">Run Server</a>
+
+
+        </body>
+    </html>
+
+
